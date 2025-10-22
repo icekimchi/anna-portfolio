ac7cece6cef23ffad8c9088c963dea49383ebd03
icekimchi <pjh010828@gmail.com>
2025-10-22 11:58:04 -0700

    feat(ProjectCard): add ProjectCard with basic layout and content

---
+++ ap-portfolio/src/components/ProjectCard.tsx
@@ -0,0 +1,38 @@
+import { Span } from "next/dist/trace";
+import Image from "next/image";
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}
+
+export default function ProjectCard({ title, description, link, image }: ProjectCardProps) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block border border-gray-200 rounded-2xl overflow-hidden hover:shadow-lg transition transform hover:-translate-y-1 bg-white"
+    >
+      {/* Image */}
+      <Image
+        src={image}
+        alt={title}
+        width={600}
+        height={400}
+        className="w-full h-48 object-cover"
+      />
+
+      {/* Text */}
+      <div className="p-5">
+        <h3 className = "text-x1 font-semibold mb-2">{title}</h3>
+        <p className="text-gray-600-mb-4">{description}</p>
+        <span className="text-blue-600 hover:underline">
+          View Project →
+        </span>
+      </div>
+    </a> 
+  );
+}
